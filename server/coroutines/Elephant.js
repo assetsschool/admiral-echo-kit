74f6c083020ff8b9ead646509eca55281c7f5161
Iain Moncrief <iainmoncrief@gmail.com>
2020-02-19 19:31:29 -1000

    testing something

---
+++ server/coroutines/Elephant.js
@@ -0,0 +1,23 @@
+const echokit = require('../../Echokit')
+
+const memories = []
+
+const remember = (envelope) => {
+    const rememberance = envelope.slot('memory')
+    memories.push(remember)
+    return echokit.makeMessage('Putting it in the ol cranium.')
+}
+
+const recall = () => {
+    if (memories.length < 1) return echokit.makeMessage('No memories recorded.')
+    let message = ''
+    for (const memory of memories) {
+        message += `${memory}. `
+    }
+    return echokit.makeMessage(message)
+}
+
+module.exports = {
+    remember: remember,
+    recall: recall
+}
--- server/coroutines/include.js
+++ server/coroutines/include.js
@@ -3,4 +3,8 @@ const coroutines = { }
 coroutines['ExampleIntent'] = require('./Example')
 coroutines['WhereIsIntent'] = require('./WhereIs')
 
+const memoryCoroutines = require('./Elephant')
+coroutines['RememberIntent'] = memoryCoroutines.remember
+coroutines['RecallIntent'] = memoryCoroutines.recall
+
 module.exports = coroutines
